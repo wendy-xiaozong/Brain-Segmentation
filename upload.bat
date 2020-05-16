4fbf6172cc3f951c7449cbc734e20a3c730e8c37
wendy-xiaozong <543443859@qq.com>
2020-05-16 16:22:18 -0300

    message

--- upload.bat
+++ upload.bat
@@ -1,3 +1,4 @@
+dos2unix C:\Users\54344\PycharmProjects\MRBrainS18\Code\runs.sh
 scp -r C:\Users\54344\PycharmProjects\MRBrainS18\Code\* jueqi@cedar.computecanada.ca:/home/jueqi/projects/def-jlevman/jueqi/MRBrainS18/Code
 git init
 git add -A C:\Users\54344\PycharmProjects\MRBrainS18\Code\*
