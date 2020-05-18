ac95f503d7e2e1f64624646d62d4f291fcd99bbb
wendy-xiaozong <543443859@qq.com>
2020-05-18 05:02:10 -0300

    message

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -4,9 +4,9 @@ import numpy as np
 
 def batch_norm_3d(inputs, name=None):
     with tf.name_scope(name):
-        print("before input:", inputs)
+        print("before input:", inputs.shape.as_list())
         batch = tf.transpose(inputs, perm=[1, 2, 3, 0, 4])
-        print("after input:", inputs)
+        print("after input:", inputs.shape.as_list())
         mean, var = tf.nn.moments(batch, axes=[0, 1, 2])
         batch = tf.nn.batch_normalization(batch,
                                           mean=mean,
