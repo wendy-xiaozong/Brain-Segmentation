c4792aca1e71810274bf6922803eb249cabd6fbe
wendy-xiaozong <543443859@qq.com>
2020-05-21 05:22:31 -0300

    message

--- upload.bat
+++ upload.bat
@@ -1,4 +1,4 @@
-scp -r C:\Users\54344\PycharmProjects\MRBrainS18\Code\* jueqi@cedar.computecanada.ca:/home/jueqi/projects/def-jlevman/jueqi/MRBrainS18/Code
+scp -r C:\Users\54344\PycharmProjects\MRBrainS18\Code\* jueqi@cedar.computecanada.ca:/home/jueqi/projects/def-jlevman/jueqi/MRBrainS18/Brain-Segmentation/
 git init
 git add -A C:\Users\54344\PycharmProjects\MRBrainS18\Code\*
 git commit -m "message"
