b0c91d469c1377d7bafd58b1597739e319bbf11d
wendy-xiaozong <543443859@qq.com>
2020-05-22 01:23:52 -0300

    message

--- utils/model_3D_Patchwise_Unet.py
+++ utils/model_3D_Patchwise_Unet.py
@@ -12,40 +12,49 @@ from .losses import get_loss
 FLAGS = tf.compat.v1.flags.FLAGS
 
 
+def get_CNN_layer(model, number_of_units, channels, name):  # block
+    for n in range(number_of_units):
+        model = Conv3D(filters=channels, kernel_size=3, strides=1, padding="same", dilation_rate=1, activation='relu',
+                       kernel_regularizer=regularizers.l2(0.5), bias_regularizer=regularizers.l2(0.5),
+                       name=name + "/conv" + str(n))(model)
+        model = BatchNormalization()(model)
+    return model
+
+
 def cnn_3d_segmentation(channels,
-                        pool_strides):
+                        pool_strides,
+                        encoder_units,
+                        decoder_units):
     """levels=3,
                               channels=[64, 128, 256],
                               encoder_units=[3, 4, 5],
                               decoder_units=[2, 2],
                               pool_strides=[[2, 2, 2], [1, 2, 2]]"""
     # encoder
+    name = 'encoder'
     inputs = Input(shape=(8, 24, 24, 3))  # ??
-    conv1 = Conv3D(filters=channels[0], kernel_size=3, strides=1, padding="same", dilation_rate=1, activation='relu',
-                   kernel_regularizer=regularizers.l2(0.5),
-                   bias_regularizer=regularizers.l2(0.5), name='encoder_1')(inputs)
-    conv1 = BatchNormalization()(conv1)  # ??
+    conv1 = get_CNN_layer(model=inputs, number_of_units=encoder_units[0], channels=channels[0], name=name)
     conv2 = AveragePooling3D(pool_size=pool_strides[0], strides=pool_strides[0], padding="same")(conv1)
-    conv2 = Conv3D(filters=channels[1], kernel_size=3, strides=1, padding="same", dilation_rate=1, activation='relu',
-                   kernel_regularizer=regularizers.l2(0.5),
-                   bias_regularizer=regularizers.l2(0.5), name='encoder_2')(conv2)
+    conv2 = get_CNN_layer(model=conv2, number_of_units=encoder_units[1], channels=channels[1], name=name)
     conv3 = AveragePooling3D(pool_size=pool_strides[1], strides=pool_strides[1], padding="same")(conv2)
-    conv3 = BatchNormalization()(conv3)
-    conv3 = Conv3D(filters=channels[2], kernel_size=3, strides=1, padding="same", dilation_rate=1, activation='relu',
-                   kernel_regularizer=regularizers.l2(0.5),
-                   bias_regularizer=regularizers.l2(0.5), name='encoder_3')(conv3)
-    conv3 = BatchNormalization()(conv3)
+    conv3 = get_CNN_layer(model=conv3, number_of_units=encoder_units[2], channels=channels[2], name=name)
+
     # decoder
-    upconv4 = Conv3DTranspose(filters=channels[2], kernel_size=pool_strides[1], strides=pool_strides[1], padding="same",
+    name = 'decoder'
+    upconv4 = Conv3DTranspose(filters=channels[1], kernel_size=pool_strides[1], strides=pool_strides[1], padding="same",
                               dilation_rate=1, activation='relu',
                               kernel_regularizer=regularizers.l2(0.5),
                               bias_regularizer=regularizers.l2(0.5), name='decoder_2')(conv3)
-    merge4 = Concatenate(axis=1)([conv2, upconv4])
-    upconv5 = Conv3DTranspose(filters=channels[1], kernel_size=pool_strides[0], strides=pool_strides[0], padding="same",
+    upconv4 = BatchNormalization()(upconv4)
+    upconv4 = get_CNN_layer(model=upconv4, number_of_units=decoder_units[1], channels=channels[])
+
+
+    merge4 = Concatenate(axis=4)([conv2, upconv4])  # ?? axis ?
+    upconv5 = Conv3DTranspose(filters=channels[0], kernel_size=pool_strides[0], strides=pool_strides[0], padding="same",
                               dilation_rate=1, activation='relu',
                               kernel_regularizer=regularizers.l2(0.5),
                               bias_regularizer=regularizers.l2(0.5), name='decoder_1')(merge4)
-    merge5 = Concatenate(axis=1)([conv1, upconv5])
+    merge5 = Concatenate(axis=4)([conv1, upconv5])
     conv6 = Conv3D(merge5, filters=11, kernel_size=1, strides=1, padding="same",
                    kernel_regularizer=regularizers.l2(0.5), bias_regularizer=regularizers.l2(0.5),
                    name='FullyConnectedLayer')
@@ -55,6 +64,9 @@ def cnn_3d_segmentation(channels,
 
 
 def build_model():
+    transition_channels = list((np.array([[64, 128, 256]]) * 0.25).astype(np.int32))
+    print("transition_channels: ", transition_channels)
+
     model = cnn_3d_segmentation(channels=[64, 128, 256],
                                 pool_strides=[[2, 2, 2], [1, 2, 2]])
     model.summary()
